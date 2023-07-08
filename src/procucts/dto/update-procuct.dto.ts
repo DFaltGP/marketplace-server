bc14ed33d15c603f49460f389b081cb1a0cd5434
DFaltGP <skyzzlockzz@gmail.com>
2023-07-08 20:01:42 -0300

    User CRUD created

--- src/procucts/dto/update-procuct.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProcuctDto } from './create-procuct.dto';
-
-export class UpdateProcuctDto extends PartialType(CreateProcuctDto) {}
--- src/procucts/procucts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProcuctsService } from './procucts.service';
-import { ProcuctsController } from './procucts.controller';
-
-@Module({
-  controllers: [ProcuctsController],
-  providers: [ProcuctsService]
-})
-export class ProcuctsModule {}
