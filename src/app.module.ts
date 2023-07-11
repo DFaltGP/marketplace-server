5c3e21d34b1878bc6e7651846df7db54b1795cd8
DFaltGP <skyzzlockzz@gmail.com>
2023-07-11 20:13:45 -0300

    feat(auth): implement login, guard & localStrategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { AccessModule } from './access/access.module';
 import { StoreModule } from './store/store.module';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AccessModule, StoreModule, ProductModule],
+  imports: [UserModule, AccessModule, StoreModule, ProductModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
