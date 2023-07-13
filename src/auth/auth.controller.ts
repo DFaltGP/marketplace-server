db0b13e3ae40a7f1cac3f5d49145f12ef22c0705
DFaltGP <skyzzlockzz@gmail.com>
2023-07-13 20:12:16 -0300

    feat(auth) make login public with custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,10 +9,12 @@ import {
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
+import { Public } from './decorators/is-public.decorator';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
